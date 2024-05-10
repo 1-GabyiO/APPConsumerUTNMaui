0a7c85a7d149276dbbe8c3ad72c861105fe37c7d
1-GabyiO <106556297+1-GabyiO@users.noreply.github.com>
2024-05-09 22:10:33 -0500

    Agregar archivos de proyecto.

---
+++ APPConsumerUTNMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace APPConsumerUTNMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
